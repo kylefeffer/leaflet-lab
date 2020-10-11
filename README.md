d0327e0ac745011da4341cde94d26689eba0e9ce
Kyle Feffer <feffer.kyle@gmail.com>
2020-10-11 18:58:45 -0500

    MAJOR CHANGE - Added data, retrieve, and sequence operators

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # leaflet-lab
 That repository is designed to  hold the first lab from geo575 through the university of Wisconsin-Madison
+
+
+
+
+Skip by MHD AZMI DWIPRANATA from the Noun Project
--- js/main.js
+++ js/main.js
@@ -1,45 +1,221 @@
-/* Map of GeoJSON data from MegaCities.geojson */
+/* Map of GeoJSON data from airports_origin.geojson */
 
 //function to instantiate the Leaflet map
-
+function createMap(){
     //create the map
     var map = L.map('map', {
-        center: [20, 0],
-        zoom: 2
+        center: [37.8, -96],
+        zoom: 3
     });
 
     //add OSM base tilelayer
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> | <a href="https://www.transtats.bts.gov/Data_Elements.aspx">Bureau of Transportation Statistics</a>'
     }).addTo(map);
 
+    //call getData function
+    getData(map);
+};
 
+//Import GeoJSON data
+function getData(map){
+    //load the data
+    $.ajax("data/airports_origin.geojson", {
+        dataType: "json",
+        success: function(response){
+            var attributes = processData(response);
+            //call function to create proportional symbols
+            createPropSymbols(response, map, attributes);
+            //call function to create slider
+            createSequenceControls(map, attributes);
+        }
+    });
+};
 
-function jsAjax(){
-    // Step 1: Create the request 
-    var ajaxRequest = new XMLHttpRequest();
+//Above Example 3.8...Step 3: build an attributes array from the data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
 
-    //Step 2: Create an event handler to send received data to a callback function
-    ajaxRequest.onreadystatechange = function(){
-        if (ajaxRequest.readyState === 4){
-            callback(ajaxRequest.response);
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("yy") > -1){
+            attributes.push(attribute);
         };
     };
 
-    //Step 3: Open the server connection
-    ajaxRequest.open('GET', 'data/MegaCities.geojson', true);
+    //check result
+    console.log(attributes);
 
-    //Step 4: Set the response data type
-    ajaxRequest.responseType = "json";
-
-    //Step 5: Send the request
-    ajaxRequest.send();
+    return attributes;
 };
 
-//define callback function
-function callback(response){
-    //tasks using the data go here
-    console.log(response);
+function createSequenceControls(map, attributes){
+    //create range input element (slider)
+    $('#panel').append('<input class="range-slider" type="range">');
+
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 20,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+
+    //below Example 3.4...add skip buttons
+    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
+    $('#panel').append('<button class="skip" id="forward">Skip</button>');
+
+    $('#reverse').html('<i class="fas fa-backward"></i>');
+    $('#forward').html('<i class="fas fa-forward"></i>');
+
+    //Example 3.12 line 7...Step 5: input listener for slider
+    $('.range-slider').on('input', function(){
+        //Step 6: get the new index value
+        var index = $(this).val();
+        console.log(index)
+
+        //Called in both skip button and slider event listener handlers
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(map, attributes[index]);
+    });
+
+        //Example 3.12 line 2...Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 20 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 20 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+
+        //Called in both skip button and slider event listener handlers
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(map, attributes[index]);
+    });
 };
 
-window.onload = jsAjax();
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 25;
+    //area based on attribute value and scale factor
+    var area = Math.abs(attValue) * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+	return radius;
+};
+
+
+ //function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Step 4: Assign the current attribute based on the first index of the attributes array
+    var attribute = attributes[0];
+    //check
+    console.log(attribute);
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>Airport:</b> " + feature.properties.Airport + "</p>";
+    popupContent += "<p><b>IATA Code:</b> " + feature.properties.IATA_Code + "</p>";
+    popupContent += "<p><b>City:</b> " + feature.properties.City + "</p>";
+
+    //add formatted attribute to popup content string
+    var month = attribute.substring(0,3);
+    var year = attribute.substring(3,5);
+    popupContent += "<p><b>Year Over Year Change (%) in  " + month + ' 20' + year + ":</b> " +  Number(Math.round(feature.properties[attribute] + 'e3') + 'e-3').toString() + "</p>";
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+    	offset: new L.Point(0, -options.radius)
+    });
+
+ ///event listeners to open popup on hover and fill panel on click...Example 2.5 line 4
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        },
+        click: function(){
+            $("#panel").html(popupContent);
+        }
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Example 2.1 line 34...Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+       //Example 3.16 line 4
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>Airport:</b> " + props.Airport + "</p>";
+            popupContent += "<p><b>IATA Code:</b> " + props.IATA_Code + "</p>";
+            popupContent += "<p><b>City:</b> " + props.City + "</p>";
+
+            //add formatted attribute to panel content string
+            var month = attribute.substring(0,3);
+            var year = attribute.substring(3,5);
+            popupContent += "<p><b>Year Over Year Change (%) in " + month + ' 20' + year + ":</b> " + Number(Math.round(props[attribute] + 'e3') + 'e-3').toString() + "</p>";
+
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
+};
+
+
+$(document).ready(createMap);
